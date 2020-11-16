c64f3c79e73b49a85c9c7cdec504ff6bc72e01f2
Zhirong Wang <zwang40@mail.ccsf.edu>
2020-11-16 04:24:18 -0800

    Updated Map --Ying

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -12,6 +12,7 @@ import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import {useHistory} from 'react-router-dom'
 import {useForm} from 'react-hook-form'
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 
 /**
 This page should contain:
@@ -27,13 +28,22 @@ const Tab1: React.FC = () => {
   const onSubmit = ()=> {
     history.push('/Symptoms')
   };
+
+  const onSubmit2 = ()=> {
+    history.push('/TestingSite')
+  };
+
+
   const location = {
   address: '1600 Amphitheatre Parkway, Mountain View, california.',
   lat: 37.42216,
   lng: -122.08427,
   }
-  const zoomLevel = 1; 
+
+  const zoomLevel = 10; 
   const myKEY = "AIzaSyBj9b-EHxuAAihd8u2HBBqWOSXukFlA3jY"
+
+  const myObject = 123; 
   return (
     <IonPage>
 
@@ -44,9 +54,17 @@ const Tab1: React.FC = () => {
       </IonHeader>
 
       <IonContent>
+
         <form onSubmit = {handleSubmit(onSubmit)}>
           <IonButton type="submit">Symptoms Checklist</IonButton>
         </form>
+
+
+        <form onSubmit = {handleSubmit(onSubmit2)}>
+          <IonButton type="submit">Find Available Testing Site</IonButton>
+        </form>
+
+        <div>Current Location: {JSON.stringify(location.lat)} ,  {JSON.stringify(location.lng)}</div>
 
         <div className="google-map">
             <GoogleMapReact
@@ -54,9 +72,8 @@ const Tab1: React.FC = () => {
               defaultCenter={location}
               defaultZoom={zoomLevel}
             >
-
             </GoogleMapReact>
-    </div>
+        </div>
 
 
 
