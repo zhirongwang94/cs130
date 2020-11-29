259aa6b647164899d561c2440b4b73eb2905eabe
shuo <shawnyang@Shawns-MBP.lan>
2020-11-29 03:29:11 -0800

    finish the cases&death feature

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -8,13 +8,23 @@ import {
     IonCardTitle, IonContent,
     IonHeader,
     IonImg,
-    IonPage, IonTitle, IonToolbar
+    IonPage, IonTitle, IonToolbar,
+    IonBadge,
+    IonItem,
+    IonLabel,
+    IonListHeader,
+    IonList,
+    IonRouterLink
 } from "@ionic/react";
 
-class newsFeed extends React.Component<{}, { Posts: string }>{
+class newsFeed extends React.Component<{}, { Posts: string}>{
+    cases: number
+    deaths: number
     constructor(props){
         super(props);
         this.state = {Posts:''};
+        this.cases = 0;
+        this.deaths = 0;
     }
     renderPosts = async() => {
         console.log("hello world")
@@ -26,7 +36,7 @@ class newsFeed extends React.Component<{}, { Posts: string }>{
                 <IonCard>
                     <IonImg src={item.image}/>
                     <IonCardHeader>
-                        <IonCardTitle>{item.title}</IonCardTitle>
+                        <IonCardTitle><IonRouterLink href={item.url}>{item.title}</IonRouterLink></IonCardTitle>
                         <IonCardSubtitle>{item.description}</IonCardSubtitle>
                     </IonCardHeader>
                     <IonCardContent>
@@ -38,8 +48,18 @@ class newsFeed extends React.Component<{}, { Posts: string }>{
         // console.log(state)
         // console.log("hello world")
     }
+    renderCases = async() => {
+        console.log("hello world")
+        let res = await axios({url: "http://127.0.0.1:3313/feed/refresh?loc=Los+Angeles",
+            method: 'get'});
+        this.cases = res.data.cases;
+        this.deaths = res.data.deaths;
+        console.log(res.data.cases);
+        console.log(res.data.deaths);
+    }
     componentDidMount() {
         this.renderPosts();
+        this.renderCases();
     }
     render() {
         return (
@@ -49,7 +69,18 @@ class newsFeed extends React.Component<{}, { Posts: string }>{
                         <IonTitle>NewsFeed</IonTitle>
                     </IonToolbar>
                 </IonHeader>
+
                 <IonContent>
+                    <IonList>
+                        <IonItem>
+                            <IonLabel>Current Cases</IonLabel>
+                            <IonBadge color="warning" >{this.cases}</IonBadge>
+                        </IonItem>
+                        <IonItem>
+                            <IonLabel>Deaths</IonLabel>
+                            <IonBadge color="danger" >{this.deaths}</IonBadge>
+                        </IonItem>
+                    </IonList>
                     {this.state.Posts}
                 </IonContent>
             </IonPage>
