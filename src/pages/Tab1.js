62b02f7dc93c65517065301911ab0a4e407d8cef
Yueying <jyy2018apply@gmail.com>
2020-12-06 22:58:34 -0800

    add test for tab1

--- src/pages/Tab1.js
+++ src/pages/Tab1.js
@@ -3,14 +3,14 @@ import GoogleMapReact from 'google-map-react';
 import './map.css'  // for the map, className="google-map"
 import { Icon } from '@iconify/react'  // for map pin
 //import locationIcon from '@iconify/icons-mdi/map-marker' // for map pin
-import PlaceCard from '../components/PlaceCard';
+//import PlaceCard from '../components/PlaceCard';
 import Marker from './Marker';
 import SimpleMarker from './SimpleMarker';
-
+import firebase from '../Firebase';
 //import { Geolocation, Geoposition } from '@ionic-native/geolocation';
-import { Button, Input, Divider, message } from 'antd';
-import { AnyCnameRecord } from 'dns';
-
+//import { Button, Input, Divider, message } from 'antd';
+//import { AnyCnameRecord } from 'dns';
+import {Plugins, LocalNotificationEnabledResult, LocalNotificationActionPerformed, Device} from '@capacitor/core';
 import {
     IonCard,
     IonCardContent,
@@ -19,17 +19,20 @@ import {
     IonCardTitle, IonContent,
     IonHeader,
     IonImg,
-    IonPage, IonTitle, IonToolbar,
+    IonPage, 
+    IonTitle, 
+    IonToolbar,
     IonBadge,
     IonItem,
     IonLabel,
-    IonListHeader,
-    IonList,
+    IonInput,
     IonButton
-} from "@ionic/react";
+  } from "@ionic/react";
+import App from '../App';
 
-import { Plugins } from '@capacitor/core';
-const { Geolocation } = Plugins;
+
+const {LocalNotifications} = Plugins;
+
 const lats = [37.7313933, 37.7413933, 37.7413933]
 const LocationPin = () => (
 <div>
@@ -44,8 +47,8 @@ class Tab1 extends Component  {
         super(props);
         this.state = {
             mapsLoaded: false,     
-            latitude:60,
-            longitude:-100,
+            latitude:34,
+            longitude:-118,
             map: {},
             mapsApi: {},
             markers: [],
@@ -65,7 +68,19 @@ class Tab1 extends Component  {
             siteLng3: -118,
             siteLng4: -118,
         };
+        this.initNotif();
+        this.notif_timeout = new Date().getTime()-10000;
     }
+
+    /**
+     * Asyncronous call to request device permissions for Local Notifications
+     * @remarks
+     * Call method before any calls that send local notifications.
+     */
+    async initNotif(){
+      await LocalNotifications.requestPermission();
+    }
+
     apiHasLoaded = ((map, mapsApi) => {
         this.setState({
           mapsLoaded: true,
@@ -76,16 +91,25 @@ class Tab1 extends Component  {
           directionService: new mapsApi.DirectionsService(),
         });
     });
-    
-
+    /**
+     * call to testing site search. It contained the main logic ofform a placesearch request 
+     * with current position and use Google.textsearch to getall place information 
+     * saved in searchResults. And render the searchResults for visualization.
+     * @remarks
+     * Call method after push the search bottom.
+     * @param {float} location.latitude - latitude value in degrees
+     * @param {float} location.longitude - longitude value in degrees
+     * @returns {Array} searchResults - top 5 testing location information
+     */
     handleSearch = (() => {
       if (this.state.map === {}){
         return
       }
-      console.log(this.state.mapsApi);
-      console.log(this.state.map);
-      console.log(this.state.placesService);
-      console.log(this.state.latitude, this.state.longitude);
+      
+      // console.log(this.state.mapsApi);
+      // console.log(this.state.map);
+      // console.log(this.state.placesService);
+      // console.log(this.state.latitude, this.state.longitude);
         let {markers, placesService, directionService,mapsApi } = this.state;
         const filteredResults = [];
         const testing_site_latlng_array = [];
@@ -101,9 +125,9 @@ class Tab1 extends Component  {
               query: 'covid 19 testing site',
               rankBy: mapsApi.places.RankBy.DISTANCE,
           }
-          console.log(placesRequest);
+          //console.log(placesRequest);
           placesService.textSearch(placesRequest, ((response) => {
-              console.log(response.length)
+              //console.log(response.length)
               let responseLimit = Math.min(5, response.length);
               for (let i = 0; i < responseLimit; i++) {
                   const covidTesting = response[i];
@@ -155,8 +179,8 @@ class Tab1 extends Component  {
                   ))
                   });
               }
-              console.log(this.state.searchResults);  
-
+              //console.log(this.state.searchResults);  
+				
               this.setState({siteLat0: filteredResults[0].lat})
               console.log(this.state.siteLat0);
               this.setState({siteLng0: filteredResults[0].lng})
@@ -176,22 +200,163 @@ class Tab1 extends Component  {
           }
           ))
     });
-    
+    /**
+     * Asyncronous call to request device permissions for Local current location and saved in state
+     * @remarks
+     * Call method when generating the scripts.
+     */
     async componentDidMount() {
       await navigator.geolocation.getCurrentPosition(
-          position => this.setState({ 
+          position => {this.setState({ 
               latitude: position.coords.latitude, 
               longitude: position.coords.longitude
-          })
-      )
-  }
+          });
+          //console.log('Position\n', position);
+        }
+      );
 
+      var id = navigator.geolocation.watchPosition(
+        (position) => {
+          const userLoc = {latitude:position.coords.latitude, longitude: position.coords.longitude};
+          //console.log("Location\n", userLoc);
+          const db = firebase.firestore().collection('users');
+          const user = firebase.auth().currentUser;
+          var user_id = '';
+          if (user!=null){
+            user_id = user.uid.toString();
+            db.doc(user_id).set({'location': userLoc});
+            this.nearPositive(userLoc, 0.01);
+          }
+        },
+        (err)=>{
+          if(err.code == 1) {
+            console.log("Error: Access is denied!");
+          } else if( err.code == 2) {
+            console.log("Error: Position is unavailable!");
+          }
+        }, 
+        { enableHighAccuracy:false ,timeout: 60000, maximumAge:30000}
+      );
+    }
 
-//    handleClick = () => {
-//        this.componentDidMount();
-//        console.log("set longitude:",this.state.longitude);
-//      }
-      
+    /**
+     * Determines if there is another user near the point 
+     * represented by param location and sends local notification to user.
+     * 
+     * @remarks
+     * Queries firestore database for all user locations
+     * Uses distance(lat1, lon1, lat2, lon2) to determine distance between users
+     * 
+     * @param {Object} location - representation of a point in the Geographic Coordinate System
+     * @param {float} location.latitude - latitude value in degrees
+     * @param {float} location.longitude - longitude value in degrees
+     * @param {float} max_dist - maximum distance between users to be considered close
+     * 
+     * @returns {number} number of positive tested users considered close to current user
+     */
+    nearPositive(location, max_dist){
+      const all_usrs = firebase.firestore().collection('users');
+      const cur_user = firebase.auth().currentUser;
+      //console.log("\n");
+      let close_usrs = [];
+      let close_usrs_len = 0;
+      if(cur_user != null){
+      //grabs all users with the same lat and long degree
+        //console.log('usrs', all_usrs.get().doc);
+        all_usrs.get().then(snapshot=>{
+          snapshot.forEach(usr=>
+            {
+              let loc = usr.get('location');
+              if(loc != undefined){
+                //let loc = usr.data('location');
+                if(!usr.isEqual(cur_user)){
+                  console.log('distance:', this.distance(loc.latitude, loc.longitude, location.latitude, location.longitude));
+                  if(this.distance(loc.latitude, loc.longitude, location.latitude, location.longitude) < max_dist){
+                    //this is not pushing , object types
+                    // WRITTEN POSSIBLY NOT FUNC CODE
+                    if(usr.get('testResult') == 'positive'){
+                      close_usrs_len=close_usrs_len+1;
+                    }
+                  }
+                }
+              }
+            });
+          
+        }).then(() => {
+          console.log("len",close_usrs_len);
+          if(close_usrs_len > 0){
+            //   //check if users are positive
+            //   //implement a time out just to make sure there is no spam
+            let cur_time = new Date().getTime();
+            if(Math.abs(cur_time - this.notif_timeout) > 10000){
+              this.notify();
+              this.notif_timeout = cur_time;
+              return close_usrs_len;
+            }
+          }
+        });
+      }
+    }
+
+    /**
+     * Asynchronously sends local notification using capacitor plugin LocalNotifications
+     * 
+     * @remarks
+     * Must call LocalNotifications.requestPermission() before method call
+     */
+    async notify(){
+      await LocalNotifications.schedule({
+        notifications:[{
+          title: 'Covid Proximity!',
+          body: 'There is a Covid positive user near your location!',
+          id: 1,
+          sound: null,
+          iconColor: '#0000FF'
+          }
+        ]
+      });
+    }
+
+    /**
+     * Converts a latitude-longitude degree to radians
+     * 
+     * @remarks
+     * Helper function for distance function
+     * 
+     * @param {float} deg - coordinate in degrees
+     * @returns {number} radian of 'deg'
+     */
+    degtorad(deg){
+      return deg * (Math.PI/180);
+    }
+
+    /**
+     * Calculates the distance between two 
+     * Geographic coordinate system points
+     * 
+     * @remarks
+     * Uses helper function - degtorad(deg)
+     * 
+     * @param {float} lat1 - latitude of the first point
+     * @param {float} lon1 - longitude of the first point
+     * @param {float} lat2 - latitude of the second point
+     * @param {float} lon2 - longitude of the first point
+     * @returns {float} distance between the points (lat1, lon1) (lat2, lon2) in kilometers.
+     */
+    distance(lat1, lon1, lat2, lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = this.degtorad(lat2-lat1);  // deg2rad below
+      var dLon = this.degtorad(lon2-lon1); 
+      var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(this.degtorad(lat1)) * Math.cos(this.degtorad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c; // Distance in km
+      return d;
+    }
+    
 
       //<IonButton color="primary" onClick={this.getLocation}>{this.state.longitude ? `${this.state.latitude} ${this.state.longitude}` : "Get Location"}</IonButton>
       render(){
@@ -215,7 +380,7 @@ class Tab1 extends Component  {
                         */}
 
               <IonContent>
-              <div>Current Location: {JSON.stringify(latitude  )} ,  {JSON.stringify(longitude )}</div>           
+              {/* <div>Current Location: {JSON.stringify(this.state.latitude  )} ,  {JSON.stringify(this.state.longitude )}</div>          */}
 {/*************** Maps Section ******************************************************************************/}              
             <div className="google-map">
             <GoogleMapReact
@@ -223,7 +388,7 @@ class Tab1 extends Component  {
                 key: "AIzaSyBj9b-EHxuAAihd8u2HBBqWOSXukFlA3jY",
                 libraries: ['places', 'directions']
               }}
-              defaultZoom={12}
+              defaultZoom={5}
               //defaultCenter={{ lat: 38, lng: -118 }}
               center={{ lat: latitude, lng: longitude }}
               yesIWantToUseGoogleMapApiInternals={true}
@@ -248,7 +413,7 @@ class Tab1 extends Component  {
 
 
 {/*************** Search Button ******************************************************************************/}           
-              <IonButton color="primary" onClick={this.handleSearch}> {"search"}  </IonButton> 
+              <IonButton justify-content='center' expand="block" shape="round" color="primary" onClick={this.handleSearch}> {"search"}  </IonButton> 
 
 
 {/*************** Result Section ******************************************************************************/}
@@ -257,7 +422,7 @@ class Tab1 extends Component  {
                {searchResults}             
                </>
                : null}
-
+                
                 </IonContent>
                 </IonPage>
             );
--- src/pages/testTab1.js
+++ src/pages/testTab1.js
@@ -3,13 +3,13 @@ import GoogleMapReact from 'google-map-react';
 import './map.css'  // for the map, className="google-map"
 import { Icon } from '@iconify/react'  // for map pin
 //import locationIcon from '@iconify/icons-mdi/map-marker' // for map pin
-import PlaceCard from '../components/PlaceCard';
-import Marker from './Marker';
+//import PlaceCard from '../components/PlaceCard';
+//import Marker from './Marker';
 import SimpleMarker from './SimpleMarker';
 
 //import { Geolocation, Geoposition } from '@ionic-native/geolocation';
-import { Button, Input, Divider, message } from 'antd';
-import { AnyCnameRecord } from 'dns';
+//import { Button, Input, Divider, message } from 'antd';
+//import { AnyCnameRecord } from 'dns';
 
 import {
     IonCard,
@@ -28,22 +28,162 @@ import {
     IonButton
 } from "@ionic/react";
 
-//class Tab1 extends Component  {
-//    constructor(props) {
-//        super(props);
-//        this.state = {
- //           mapsLoaded: false,     
-//            latitude:38,
-//            longitude:-100,
-//            map: {},
-//            mapsApi: {},
-//            markers: [],
-//            searchResults: [],
- //           placesService: {},
-//            geoCoderService: {},
-//            directionService: {},
-//            testing_site_locations: [], // array of {lat: number, lng: number}
-//            
-//        };
-//    }
-//}
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
+const lats = [37.7313933, 37.7413933, 37.7413933]
+const LocationPin = () => (
+<div>
+  <SimpleMarker name="My Marker" color="blue"/>
+  <p>1</p>
+</div>
+
+)
+
+class Tab1Test extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+        mapsLoaded: false,     
+        latitude:38,
+        longitude:-100,
+        map: {},
+        mapsApi: {},
+        markers: [],
+        searchResults: [],
+        placesService: {},
+        geoCoderService: {},
+        directionService: {},
+        testing_site_locations: [], // array of {lat: number, lng: number}
+        
+    };
+}
+
+apiHasLoaded = ((map, mapsApi) => {
+    this.setState({
+      mapsLoaded: true,
+      map: map,
+      mapsApi: mapsApi,
+      placesService: new mapsApi.places.PlacesService(map),
+      geoCoderService: new mapsApi.Geocoder(),
+      directionService: new mapsApi.DirectionsService(),
+    });
+});
+
+handleSearch = (() => {
+    if (this.state.map === {}){
+      return
+    }
+    console.log(this.state.mapsApi);
+    console.log(this.state.map);
+    console.log(this.state.placesService);
+    console.log(this.state.latitude, this.state.longitude);
+      let {markers, placesService, directionService,mapsApi } = this.state;
+      const filteredResults = [];
+      const testing_site_latlng_array = [];
+      //if (markers.length === 0){
+      //  console.log("add marker and try again");
+      //  return;
+      //}
+      //const markerLatLng = new mapsApi.LatLng(this.state.latitude, this.state.longitude);
+      let locations = [];
+      // los angeles, san francisco, seattle
+      let citys = [[33.7690882,34.2763908,-118.4948469,-117.745091],[37.2802362,37.986638,-122.4835193,-121.855137],
+    [47.2031566,47.9789848,-122.3320,-122.0355736],[41.505286,42.248038,-88.283689,-87.651560]
+    ];
+      for(let i = 0; i < 4; i++){
+          for(let j = 0; j <= 10; j++){
+                const curlat = Math.random() * (citys[i][1]-citys[i][0]) + citys[i][0];
+                const curlong = Math.random()*(citys[i][3]-citys[i][2]) + citys[i][2];
+                locations.push([curlat,curlong]);
+          }
+
+      }
+      //locations.push([]);
+      const loc = [ 33.960697786372634,-118.27835814456115];
+      // m could be 0-10, 10-20, 20-30, 30-40 
+      for(let m = 30; m < 40; m++){
+      const markerLatLng = new mapsApi.LatLng(locations[m][0],locations[m][1])
+      console.log(m,locations[m][0],locations[m][1]);
+      const placesRequest = {
+            location: markerLatLng,
+            //radius: '3000000', // Cannot be used with rankBy. Pick your poison!
+            type: ['hospital', 'pharmacy','doctor'], // List of types: https://developers.google.com/places/supported_types
+            query: 'covid 19 testing site',
+            rankBy: mapsApi.places.RankBy.DISTANCE,
+        };
+        //console.log(placesRequest);
+        placesService.textSearch(placesRequest, ((response) => {
+          if(response == null){
+            console.log("response is null",response,m,locations[m][0],locations[m][1]);
+            
+          }
+          else{
+            console.log(m,response.length)
+            let responseLimit = Math.min(5, response.length);
+            for (let i = 0; i < responseLimit; i++) {
+                const covidTesting = response[i];
+                const { rating, name } = covidTesting;
+                const address = covidTesting.formatted_address; 
+                const lat = covidTesting.geometry.location.lat();
+                const lng = covidTesting.geometry.location.lng();
+                let openNow = false;
+                let photoUrl = '';
+                if (covidTesting.opening_hours) {
+                    openNow = covidTesting.opening_hours.isOpen(); // e.g true/false
+                }
+                if (covidTesting.photos && covidTesting.photos.length > 0) {
+                  photoUrl = covidTesting.photos[0].getUrl();
+                }
+                //console.log(name,address, openNow,covidTesting.opening_hours, rating);
+            }
+            }
+        }
+        ))
+    }
+    });
+    render(){
+        const { markers, geoCoderService,searchResults,latitude,longitude } = this.state;
+        return(
+            <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Testing Site Search</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+
+                  {/* 
+          <IonHeader>
+            <IonToolbar color="primary">
+              <IonTitle>Login</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+                    */}
+
+          <IonContent>
+               
+{/*************** Maps Section ******************************************************************************/}              
+        <div className="google-map">
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: "AIzaSyBj9b-EHxuAAihd8u2HBBqWOSXukFlA3jY",
+            libraries: ['places', 'directions']
+          }}
+          defaultZoom={12}
+          //defaultCenter={{ lat: 38, lng: -118 }}
+          center={{ lat: latitude, lng: longitude }}
+          yesIWantToUseGoogleMapApiInternals={true}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => this.apiHasLoaded(map, maps)} // "maps" is the mapApi. Bad naming but that's their library.
+
+        >
+
+        </GoogleMapReact>
+        </div>
+         <IonButton justify-content='center' expand="block" shape="round" color="primary" onClick={this.handleSearch}> {"search"}  </IonButton> 
+        </IonContent>
+            </IonPage>
+        )
+    }  
+};
+  
+  export default Tab1Test;
